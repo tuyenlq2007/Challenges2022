f3a53a45815cc84281ffb8c4482d5dd5be2de9ce
tuyenlq2007 <tuyenlq2007@gmail.com>
2022-03-10 09:45:31 +0700

    Update Array-subarray.js

--- Array-subarray.js
+++ Array-subarray.js
@@ -5,3 +5,18 @@ b=a.find(a=>a==d);
 console.log(b==null?"not found":b);
 }
 
+
+//find index of sub array
+arr = [4,3,2,1,2,1,1,2,3,4];
+subarr=[1,2,1,1];
+let i=0;
+let j=0;
+while(i< arr.length-1){
+let found=true;
+for (j = 0; j < subarr.length; j++) {
+    if (arr[i+j] != subarr[j]) break;
+}
+
+if (j==subarr.length) console.log (i);
+i++;
+}
