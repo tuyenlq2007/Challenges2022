1e2a4fc3cdf01b17db387a7eb368becb14cffcb9
tuyenlq2007 <tuyenlq2007@gmail.com>
2022-03-12 14:04:51 +0700

    Update String-generatesubstring.js

--- String-generatesubstring.js
+++ String-generatesubstring.js
@@ -1,3 +1,4 @@
+//option1
 a="abcd".split("")
 function generateSub(a){
     let sub=a[0];
@@ -7,5 +8,16 @@ for (let i=1;i<a.length;i++){
      console.log(sub);
 }
 if(a.length>1) generateSub(a.slice(1,a.length));
+}
+generateSub(a)
+
+//option2
+a="abcd".split("")
+function generateSub(a){
+for (let i=0;i<a.length;i++){
+     for(let j=i+1;j<a.length+1;j++){
+     console.log(a.slice(i,j));
+     }
+}
 }
 generateSub(a)
