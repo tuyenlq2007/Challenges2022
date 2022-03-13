8ea2d2ae6fe246a704f65d888b12dc4c57714543
tuyenlq2007 <tuyenlq2007@gmail.com>
2022-03-13 19:30:55 +0700

    Update Statement-this.js

--- Statement-this.js
+++ Statement-this.js
@@ -1,1 +1,17 @@
+//use with call
 
+function add(c, d) {
+  return this.a + this.b + c + d;
+}
+
+var o = {a: 1, b: 3};
+
+// The first parameter is the object to use as
+// 'this', subsequent parameters are passed as
+// arguments in the function call
+add.call(o, 5, 7); // 16
+
+// The first parameter is the object to use as
+// 'this', the second is an array whose
+// members are used as the arguments in the function call
+add.apply(o, [10, 20]); // 34
