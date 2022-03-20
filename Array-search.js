f33b3d13456940bde35cda0169f7ef6665da1c37
tuyenlq2007 <tuyenlq2007@gmail.com>
2022-03-20 13:15:55 +0700

    Update Array-search.js

--- Array-search.js
+++ Array-search.js
@@ -3,3 +3,16 @@ a=["12","21","3"]
 for (let k of a.keys()){
  if (a[k].startsWith("1")) console.log(a.splice(k,1));
 }
+//binary search
+//Binary search element in sorted array
+a=[1,2,3,4,5,6,7];
+t=3;
+l=0;
+r=a.length;
+m=Math.floor((l+r)/2)
+while (l<r && a[m]!=t){
+m=Math.floor((l+r)/2);
+if (a[m]>t) r=m;
+if (a[m]<t) l=m;
+}
+console.log(m);
