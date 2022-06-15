b0d839a8e7c28a2d759c6b44ba64c356badafb22
tuyenlq2007 <tuyenlq2007@gmail.com>
2022-06-15 12:32:53 +0100

    update testscripts

---
+++ app.js
@@ -0,0 +1,40 @@
+var per=[];
+function permute(str, l, r)
+{
+    if (l == r){
+            per.push(str);
+            return;
+    }
+
+        
+    for (let i = l; i < r; i++)
+    {
+        let newstr = swap(str, l, i);
+        permute(newstr, l + 1, r);
+    }
+
+        
+}
+ 
+
+function swap(a, i, j)
+{
+
+let temp;
+let charArray = a.split("");
+temp = charArray[i] ;
+charArray[i] = charArray[j];
+charArray[j] = temp;
+return (charArray).join("");
+}
+ 
+function main() {
+    let str = "ABC";
+    let n = str.length;
+    permute(str, 0, n);
+    console.log(per);
+}
+
+if (require.main === module) {
+    main();
+}
