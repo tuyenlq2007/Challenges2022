a4d5e77553d7af4ab1ffe7f3ec5f04ad14980924
tuyenlq2007 <tuyenlq2007@gmail.com>
2023-07-12 13:40:59 +0700

    Update DFS_Grid_ConnectedCells.js

--- DFS_Grid_ConnectedCells.js
+++ DFS_Grid_ConnectedCells.js
@@ -1,22 +1,59 @@
-grid=[[1,1,0],[0,0,0],[0,0,0]]
+//Binary search element in sorted array
+grid=[[1,1,0,0],[0,1,1,0],[0,0,1,0],[1,0,0,0]]
 
-console.log(grid[0][0]);//1
-console.log(grid[1][0]);//0
-console.log(grid[0][1]);//1
-console.log(grid[1][1]);//0
-console.log(count_related_cell(grid,0,1));
+//console.log(grid[0][0]);//1
+//console.log(grid[1][0]);//0
+//console.log(grid[0][1]);//1
+//console.log(grid[1][1]);//0
 
-function count_related_cell(grid, row, col){
-  if (row<0 || col<0|| row>grid.length||col>grid[0].length) return 0
-  if (grid[row][col]==0) return 0
-  let count=1
-  grid[row][col]=0
-  for (r=row-1; r<row+1; r++){
+console.log(largestRegion(grid))
+//console.log(count_related_cell1(grid,0,0));//
+
+function largestRegion(grid) {
+        let maxRegion = 0;
+        
+        for (row = 0; row < 4; row++) {
+            for (col = 0; col < 4; col++) {
+                /* Find the largest region from the current cell */
+                if (grid[row][col] == 1) {
+                    let size  = findLargestRegion(grid, row, col);
+                    if (size>maxRegion) maxRegion= size;
+                }
+            }
+        }
+        return maxRegion;
+    }
+    
+function findLargestRegion(grid, row, col){
+/* Put boundary checks here (at top of recursive call), instead of before doing recursive call */
+        if (row < 0 || row >= 4 || col < 0 || col >= 4 || grid == null || grid[row][col] == 0) {
+            return 0;
+        }
+
+        grid[row][col] = 0; // we alter the original matrix here
+        let size = 1;       // 1 accounts for our size
+        
+        /* Recursively search neighbors */
+        for (let r = row - 1; r <= row + 1; r++) {
+            for (let c = col - 1; c <= col + 1; c++) {
+                size += findLargestRegion(grid, r, c);
+            }
+        }
+
+        return size;
+   
+}
+
+
+function count_related_cell1(grid, row, col){
+ if (grid[row][col]==0) return 0;
+ count = 1;
+ grid[row][col]=0
+ for (r=row-1; r<row+1; r++){
      for (c=col-1; c<col+1; c++){
-         if (r!=row||c!=col)
          count=count+count_related_cell(grid, r, c)
      }
   }
-  return count;
+ return count;
    
 }
